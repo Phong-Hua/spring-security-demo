78bebcfe7c696456ce418dfc2fd6a438111d4f1c
admin <admin@admins-imac.gateway>
2022-10-19 16:38:41 +1100

    2nd commit - add basic security Loggin use default spring login form

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// We only extends AbstractSecurityWebApplicationInitializer and no need to do anything else
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
